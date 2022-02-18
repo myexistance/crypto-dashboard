833dfbf3968714cffeedf123dd08adddc57f26ca
Joon Yoon <myexistance@HOTMAIL.COM>
2022-02-19 09:20:42 +1300

    Primary Currency typo

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -4,21 +4,24 @@ const CurrencyConverter = ()  =>  {
   return (
     <div className ="currency-converter">
       <h2>Currency Converter</h2>
-      <table>
-          <body>
-                    <tr>
-                      <td>Primari Currency:</td>
-                      <td>
-                        <input
-                        type="number"
-                        name="currency-amount-1"
-                        value={""}
-                        />
-                      </td>
-                    </tr>
-          </body>
-      </table>
+      <div>
+                <table>
+                   <body>
+                             <tr>
+                               <td>Primary Currency:</td>
+                               <td>
+                                 <input
+                                 type="number"
+                                 name="currency-amount-1"
+                                 value={""}
+                                 />
+                               </td>
+                             </tr>
+                   </body>
+               </table>
 
+      </div>
+      
       <ExchangeRate/>
     </div>
   )
