cecf1ae6fc12bea89f50c1d3f04a3056d7631bde
Joon Yoon <myexistance@HOTMAIL.COM>
2022-02-19 07:38:36 +1300

    commit

---
+++ src/components/CurrencyConverter.js
@@ -0,0 +1,12 @@
+import ExchangeRate from "./components/ExchangeRate"
+
+const CurrencyConverter = ()  =>  {
+  return (
+    <div className ="currency-converter">
+      Currency Converter
+      <ExchangeRate/>
+    </div>
+  )
+}
+
+export default CurrencyConverter
