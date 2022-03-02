7742a760ebf34683a6d691cf4d6038cacc5a6be8
Joon Yoon <myexistance@HOTMAIL.COM>
2022-03-02 14:16:21 +1300

    print articles useState

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const NewsFeed = ()  =>  {
+  //prtint articles in the NewsFeed section        
   const [articles, setArticles] = useState(null)
 
   useEffect(() => {
           
-      const options = {      
-        method: 'GET',
-        url: 'https://alpha-vantage.p.rapidapi.com/query',
-        params: {from_currency: chosenPrimaryCurrency, function: 'CURRENCY_EXCHANGE_RATE',       to_currency: chosenSecondaryCurrency},
-        headers: {
-          'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
-          'x-rapidapi-key': '084895d68amshd43646cc4e216f4p1626a4jsn6bf06a1a7aae'
-        }
-      };
+      const options = {
+      method: 'GET',
+      url: 'https://crypto-news-live3.p.rapidapi.com/news',
+      headers: {
+      'x-rapidapi-host': 'crypto-news-live3.p.rapidapi.com',
+      'x-rapidapi-key':           '084895d68amshd43646cc4e216f4p1626a4jsn6bf06a1a7aae'
+      }
+      };          
 
       axios.request(options).then((response) => {
 	console.log(response.data)
